29249b5805ca9e1acaa716a3a3e1c7a7a805f92a
Keoma Lima <keomalima@outlook.com>
2024-04-18 14:42:37 +0200

    Frontend - Error screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,10 +13,11 @@ import LoginScreen from './screens/login/LoginScreen.jsx';
 import RegisterScreen from './screens/register/RegisterScreen.jsx';
 import DashboardScreen from './screens/dashboard/DashboardScreen.jsx';
 import PrivateRoute from './components/privateRoute.jsx';
+import ErrorPage from './screens/error-page.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
+    <Route path='/' element={<App />} errorElement={<ErrorPage />}>
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
       <Route path='' element={<PrivateRoute />}>
---
+++ frontend/src/screens/error-page.jsx
@@ -0,0 +1,15 @@
+import { useRouteError } from 'react-router-dom';
+
+export default function ErrorPage() {
+  const error = useRouteError();
+
+  return (
+    <div className='flex-error'>
+      <h1 className='strong md'>Oops!</h1>
+      <p className='normal'>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
