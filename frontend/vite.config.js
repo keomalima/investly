1e0b4191493c1a3010c805e77a6adc7a23f0a6d9
Keoma Lima <keomalima@outlook.com>
2024-05-22 19:06:45 -0300

    Frontend - Fixed small bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000/',
+        target: 'https://investly.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/backend-api/, ''),
       },
