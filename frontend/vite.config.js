c96752a6d1c43466b889734d50f298e56dc0bd5b
Keoma Lima <keomalima@outlook.com>
2024-05-22 20:06:17 -0300

    Frontend - Fixed a few bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['axios-cache-interceptor'],
-    },
-  },
   server: {
     port: 3000,
     proxy: {
