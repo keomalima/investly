7fc78415b9f21dbdd2af8fa64e240b2f691b0a05
Keoma Lima <keomalima@outlook.com>
2024-05-23 19:26:58 -0300

    Frontend - API proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,17 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://investly.onrender.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/backend-api/, ''),
+      },
+      '/fmp-api': {
+        target: 'https://financialmodelingprep.com/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/fmp-api/, ''),
+      },
+    },
   },
 });
