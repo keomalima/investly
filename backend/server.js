6565e6bada6bb8e16422d3c3e365bf41842a58d2
Keoma Lima <keomalima@outlook.com>
2024-05-24 11:23:13 -0300

    Backend - 2

--- backend/server.js
+++ backend/server.js
@@ -26,17 +26,13 @@ async function initializeServer() {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(cookieParser());
 
-    app.use(function (req, res, next) {
-      res.header(
-        'Access-Control-Allow-Origin',
-        'https://investly-ten.vercel.app'
-      ); // update to match the domain you will make the request from
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-      );
-      next();
-    });
+    app.use(
+      cors({
+        origin: 'https://investly-ten.vercel.app', // Your Vercel deployment domain
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+        credentials: true, // Allow cookies if needed
+      })
+    );
 
     // Sets the API route paths
     app.use('/api/users', userRoutes);
